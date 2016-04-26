7845a367617e8702bac1575320e23137e2c17711
Lance Fallon <lfallon@Lances-Mac-mini.local>
2016-04-26 12:51:25 -0400

    cleanup imports

--- app/src/main/java/com/training/lfallon/actionbarfragments/MainActivity.java
+++ app/src/main/java/com/training/lfallon/actionbarfragments/MainActivity.java
@@ -1,13 +1,8 @@
 package com.training.lfallon.actionbarfragments;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
