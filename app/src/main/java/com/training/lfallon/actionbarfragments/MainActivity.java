9b72223d1ffef333937ccc830719dd6de40575a6
Lance Fallon <lfallon@Lances-Mac-mini.local>
2016-04-26 13:57:29 -0400

    custom action bar view

--- app/src/main/java/com/training/lfallon/actionbarfragments/MainActivity.java
+++ app/src/main/java/com/training/lfallon/actionbarfragments/MainActivity.java
@@ -1,40 +1,36 @@
 package com.training.lfallon.actionbarfragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.activity_main_page1btn).setOnClickListener(this);
-        findViewById(R.id.activity_main_page2btn).setOnClickListener(this);
-    }
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setCustomView(R.layout.action_bar_custom_view);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.activity_main_page1btn){
-            switchPage(new Page1Fragment());
-        }
-        else if(view.getId() == R.id.activity_main_page2btn){
-            switchPage(new Page2Fragment());
-        }
+        final EditText editText = (EditText) actionBar.getCustomView().findViewById(R.id.action_bar_custom_view_editText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                actionBar.setTitle(editText.getText().toString());
+                actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE);
+                return true;
+            }
+        });
     }
-
-    private void switchPage(Fragment fragment) {
-        getFragmentManager().beginTransaction()
-            .replace(R.id.placeholder, fragment)
-            .commit();
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -43,10 +39,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_settings){
-            Toast.makeText(this, "Main menu item selected", Toast.LENGTH_SHORT).show();
+        int id = item.getItemId();
+
+        if(id == R.id.menu_main_settings){
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
